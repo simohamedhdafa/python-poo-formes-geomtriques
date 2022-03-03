84b8096b1810feee602b7db757fe5fb53773c8cb
simohamedhdafa <simohamed.hdafa@gmail.com>
2022-03-03 15:34:34 +0100

    Add files via upload

---
+++ partie3/Shape.py
@@ -0,0 +1,21 @@
+from turtle import *
+class Shape:
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
+
+    def getX(self):
+        return self._x
+    def getY(self):
+        return self._y
+    
+    def setX(self, x):
+        self._x = x
+    def setY(self, y):
+        self._y = y
+
+    def draw(self, t):
+        t.penup()
+        t.setx(self._x)
+        t.sety(self._y)
+        t.pendown()
---
+++ partie3/cercleTest.py
@@ -0,0 +1,13 @@
+import turtle as tt
+from Circle import *
+
+if __name__ == '__main__':
+    
+    C1 = Circle(10, 50, 100)
+    C2 = Circle(20, 30, 200)
+    # x 10 y 50 radius 100
+    #print(C1) # print(C1.__str__())
+    # x 20 y 30 radius 200
+    #print(C2)
+    C1.draw(tt)
+    C2.draw(tt)
